a4cac2cb32e017bab5bdf0096a6448364cdd0aed
Farrukh Naveed <farrukhnaveed@users.noreply.github.com>
2023-11-01 01:24:47 +0500

    removed currency symbol

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const pgp = require('pg-promise')();
 const csv = require('csv-parser');
 const dbConfig = require('./config/db');
-const { dataType, columnTranslation } = require('./config/constants');
+const { columnTranslation } = require('./config/constants');
 
 // Specify the path to your CSV file
 const folder = './csvFiles';
@@ -68,6 +68,9 @@ async function getFileContent(fileId, mapping) {
                 // console.log(`Key: ${key}, Value: ${field}`);
         
                 if (field.type === 'column') {
+                    if (['8', '25', '26'].includes(fileId) && ['total_shipment_cost'].includes(key)) {
+                        row[field.file] = row[field.file].replace(/[^0-9.-]/g, '');
+                    }
                     data[key] = row[field.file];
                 } else {
                     data[key] = field.file;
@@ -84,75 +87,97 @@ async function getFileContent(fileId, mapping) {
     return content;
 }
 
+const db = pgp(dbConfig);
+
 async function init() {
-    const db = pgp(dbConfig);
-    // const fileMapping = await getColumnMap();
+    const args = process.argv.slice(2);
 
-    await Promise.all(Object.entries(columnTranslation).map(async ([fileId, mapping]) => {
-        if (fileId !== '1') return; //TODO: temp change, testing on 1 file
-        // console.log(`Key: ${fileId}, Value: ${mapping}`);
-        const whereKey = ['17', '18'].includes(fileId) ? 'shipping_invoice_number' : 'tracking_code';
-
-        const content = await getFileContent(fileId, mapping);
-            // console.log(content);
-
-        for (let index = 0; index < content.length; index++) {
-            let result = null;
-            const row = content[index];
-
-            if (['-', '', null].includes(row[whereKey].trim())) {
-                console.log(`Invalid value found for ${whereKey} = ${row[whereKey]} not found!`);
-                continue;
-            }
-            // console.log(row);
-            const values = [row[whereKey]];
-            
-            let query = `select s.tracking_code, s."shipping_invoice_number", sc.shipment_currency , sc.total_shipment_cost,
-            sc.tax_charges , sc.fuel_charges, sc.other_costs , sc.shipping_cost , sc.surcharge_cost , sc.insurance_cost , sc.status
-            from "Shipments" s inner join "ShipmentCosts" sc on s."ShipmentCostId" = sc.id
-            where s.${whereKey} = $1`;
-
-            try {
-                result = await db.one(query, values);
-            } catch (error) {
-                console.error(`Error finding row with tracking_code = ${row.tracking_code}`, error);
-                continue;
-            }
-
-            if (result == null) {
-                console.log(`Shipment cost of tracking_code = ${row.tracking_code} not found!`);
-                continue;
-            }
-            
-            const keys = Object.keys(row).filter(column => mapping[column].table === 'ShipmentCosts' && result[column] === null && row[column] !== null);
-
-            if (keys.length === 0) {
-                console.log(`No columns to update for tracking_code = ${row.tracking_code}`);
-                continue;
-            }
-            const keyFormatted = keys.map( (key, index) => {
-                values.push(row[key]);
-                return `${key} = $${index+2}`;
-            });
-            query = `update "ShipmentCosts"
-            set ${keyFormatted.join(', ')}
-            from "Shipments"
-            where "Shipments"."ShipmentCostId" = "ShipmentCosts".id
-                and "Shipments".${whereKey} = $1;`;
-            console.log(query);
-            console.log(values);
-
-            try {
-                await db.none(query, values);
-            } catch (error) {
-                console.error(`Error updating row with tracking_code = ${row.tracking_code}`, error);
-            }
-            return;  //TODO: temp change
+    if (args.length > 0) {
+        const fileId = args[0];
+        if (fileId === 'all') {
+            processAllFiles();
+        } else if (/^[1-9]\d*$/.test(fileId) && parseInt(fileId) > 0 && parseInt(fileId) < 40) {
+            processFile(fileId, columnTranslation[fileId]);
+        } else {
+            console.log('Invalid arguments provided.');
         }
-    }));
-
-    // console.log(columnTranslation);
+    } else {
+        console.log('Invalid arguments provided.');
+    }
     db.$pool.end(); // Close the database connection
+}
+
+async function processFile(fileId, mapping) {
+    if (['15', '16', '18'].includes(fileId)) {
+        console.log(`FileID = ${fileId} not allowed!`);
+        return;
+    }
+    const whereKey = ['17', '18'].includes(fileId) ? 'shipping_invoice_number' : 'tracking_code';
+    const content = await getFileContent(fileId, mapping);
+    // console.log(content);
+
+    for (let index = 0; index < content.length; index++) {
+        let result = null;
+        const row = content[index];
+
+        if (['-', '', null].includes(row[whereKey].trim())) {
+            console.log(`Invalid value found for ${whereKey} = ${row[whereKey]} not found!`);
+            continue;
+        }
+        // console.log(row);
+        const values = [row[whereKey]];
+        
+        let query = `select s.tracking_code, s."shipping_invoice_number", sc.shipment_currency , sc.total_shipment_cost,
+        sc.tax_charges , sc.fuel_charges, sc.other_costs , sc.shipping_cost , sc.surcharge_cost , sc.insurance_cost , sc.status
+        from "Shipments" s inner join "ShipmentCosts" sc on s."ShipmentCostId" = sc.id
+        where s.${whereKey} = $1`;
+
+        try {
+            result = await db.one(query, values);
+        } catch (error) {
+            console.error(`Error finding row with tracking_code = ${row.tracking_code}`, error);
+            continue;
+        }
+
+        if (result == null) {
+            console.log(`Shipment cost of tracking_code = ${row.tracking_code} not found!`);
+            continue;
+        } else if (result.status !== 'OPEN') {
+            console.log(`Shipment cost of tracking_code = ${row.tracking_code} is not OPEN`);
+            continue;
+        }
+        
+        const keys = Object.keys(row).filter(column => mapping[column].table === 'ShipmentCosts' && row[column] !== null); //  && result[column] === null
+
+        if (keys.length === 0) {
+            console.log(`No columns to update for tracking_code = ${row.tracking_code}`);
+            continue;
+        }
+        const keyFormatted = keys.map( (key, index) => {
+            values.push(row[key]);
+            return `${key} = $${index+2}`;
+        });
+        query = `update "ShipmentCosts"
+        set ${keyFormatted.join(', ')}
+        from "Shipments"
+        where "Shipments"."ShipmentCostId" = "ShipmentCosts".id
+            and "Shipments".${whereKey} = $1;`;
+        console.log(query);
+        console.log(values);
+
+        try {
+            await db.none(query, values);
+        } catch (error) {
+            console.error(`Error updating row with tracking_code = ${row.tracking_code}`, error);
+        }
+        return;  //TODO: temp change
+    }
+}
+
+async function processAllFiles() {
+    await Promise.all(Object.entries(columnTranslation).map(async ([fileId, mapping]) => {
+        await processFile(fileId, mapping);
+    }));
 }
 
 init()
